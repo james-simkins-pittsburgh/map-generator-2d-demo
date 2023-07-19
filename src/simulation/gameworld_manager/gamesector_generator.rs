de69c4dc35b8a3fb3340569ccfec60a0ddcbd39f
jsimkins13 <jamesmsimkins@gmail.com>
2023-07-19 09:11:28 -0400

    Work on Procedural Generation
    Getting the random number generator seeded for procedural generation

--- src/simulation/gameworld_manager/gamesector_generator.rs
+++ src/simulation/gameworld_manager/gamesector_generator.rs
@@ -63,7 +63,6 @@ pub fn generate_sector(
             });
 
             make_tiles_now.ready_now.0 = true;
-            
         }
 
         // This clears the event so sectors aren't generated more than once.
@@ -79,18 +78,42 @@ fn generate_map(
     y_coordinate: i32,
     gameworld_seed_num: &u64
 ) -> ([[(TileType, u8); SECTOR_SIZE as usize]; SECTOR_SIZE as usize], SectorBiome, (i32, i32)) {
-    // This initially sets the array to vegetated.
+    // This initially sets the array to open.
     let gamesector_environment_array = [
         [(TileType::Open, 0); SECTOR_SIZE as usize];
         SECTOR_SIZE as usize
     ];
 
-    // This is just placeholder code
+    // This is just placeholder code.
     let sector_biome = SectorBiome::Base;
 
-    // Code to generate gameworld will go here.
+    // This block of code deteministically gets a sector seed from the gameworld seed.
+    // First we need to convert our coordinates to u64.
 
-    let mut _seeded_prng = rand_chacha::ChaCha8Rng::seed_from_u64(gameworld_seed_num.clone());
+    let x_coordinate_to_u64: u64 = if x_coordinate < 0 {
+        u64::MAX - (x_coordinate.abs() as u64)
+    } else {
+        x_coordinate as u64
+    };
+
+    let y_coordinate_to_u64: u64 = if y_coordinate < 0 {
+        u64::MAX - (y_coordinate.abs() as u64)
+    } else {
+        y_coordinate as u64
+    };
+
+    // Now we add a big number based on the coordinated to the gameworld seed number.
+    // If this makes it larger than u64 that's fine since we just wrapping_add it.
+    // 412 is for Pittsburgh pride! The other numbers are just large primes.
+
+    let sector_seed_num: u64 = gameworld_seed_num.wrapping_add(
+        412 + 3943 * x_coordinate_to_u64 + 4211 * y_coordinate_to_u64
+    );
+
+    // Now we seed the random number generator.
+
+    let seeded_prng = rand_chacha::ChaCha8Rng::seed_from_u64(sector_seed_num);
+
 
     (gamesector_environment_array, sector_biome, (x_coordinate, y_coordinate))
 }

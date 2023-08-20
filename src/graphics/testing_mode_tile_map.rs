c489097a19e6e66ac78915d94dd8aeed86edb322
jsimkins13 <jamesmsimkins@gmail.com>
2023-08-20 09:32:28 -0400

    Updated Tile Map Drawing
    Now it is possible to update the tile map without spawning new tiles.

--- src/graphics/testing_mode_tile_map.rs
+++ src/graphics/testing_mode_tile_map.rs
@@ -10,13 +10,16 @@ pub struct EnvironmentalTextureHandle {
 
 #[derive(Resource, Default)]
 pub struct TileControlForSectorSwitch {
-    
     pub gamesector_generated: bool,
     pub gamesector_copied: bool,
-    pub gamesector_drawn: bool, 
-
+    pub gamesector_drawn: bool,
 }
 
+#[derive(Component)]
+pub struct TileIndex {
+    pub x: u16,
+    pub y: u16,
+}
 
 pub fn tile_texture_loader(
     asset_server: Res<AssetServer>,
@@ -25,82 +28,120 @@ pub fn tile_texture_loader(
     env_texture_handle.handle = asset_server.load("environment.png");
 }
 
-pub fn testing_mode_tile_map(
-    mut tile_control: ResMut<TileControlForSectorSwitch>,
-    mut commands: Commands,
-    graphics_memory_sector_query: Query<&crate::graphics::GamesectorGraphicsBasicsMemory>,
+pub fn testing_mode_spawn_tile_map(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    env_texture_handle: Res<EnvironmentalTextureHandle>
+    env_texture_handle: Res<EnvironmentalTextureHandle>,
+    mut commands: Commands
 ) {
-    if tile_control.gamesector_generated && tile_control.gamesector_copied && !tile_control.gamesector_drawn {
-        let env_texture_atlas = TextureAtlas::from_grid(
-            env_texture_handle.handle.clone(),
-            Vec2::new(96.0, 96.0),
-            10,
-            10,
-            Some(Vec2::new(6.0, 6.0)),
-            Some(Vec2::new(3.0, 3.0))
-        );
+    let env_texture_atlas = TextureAtlas::from_grid(
+        env_texture_handle.handle.clone(),
+        Vec2::new(96.0, 96.0),
+        10,
+        10,
+        Some(Vec2::new(6.0, 6.0)),
+        Some(Vec2::new(3.0, 3.0))
+    );
 
-        let env_texture_atlas_handle = texture_atlases.add(env_texture_atlas);
+    let env_texture_atlas_handle = texture_atlases.add(env_texture_atlas);
+
+    let mut sprite_transform: Transform;
+
+    for x_index in 0..SECTOR_SIZE {
+        for y_index in 0..SECTOR_SIZE {
+            sprite_transform = Transform::from_xyz(
+                (((x_index as f32) - 50.0) * 96.0) as f32,
+                (((y_index as f32) - 50.0) * 96.0) as f32,
+                0.0
+            );
+
+            commands.spawn((
+                SpriteSheetBundle {
+                    sprite: TextureAtlasSprite::new(0),
+                    texture_atlas: env_texture_atlas_handle.clone(),
+                    transform: sprite_transform,
+                    ..default()
+                },
+                TileIndex { x: x_index, y: y_index },
+            ));
+        }
+    }
+}
+
+pub fn testing_mode_update_tile_map(
+    mut tile_control: ResMut<TileControlForSectorSwitch>,
+    graphics_memory_sector_query: Query<&crate::graphics::GamesectorGraphicsBasicsMemory>,
+    mut tile_query: Query <(&mut TileIndex, &mut TextureAtlasSprite, &mut Transform)>
+) {
+    if
+        tile_control.gamesector_generated &&
+        tile_control.gamesector_copied &&
+        !tile_control.gamesector_drawn
+    {
         let mut sprite_transform: Transform;
+        let mut tile_graphics_index: u16;
 
         for graphics_sector_memory in graphics_memory_sector_query.iter() {
-            #[allow(unused_assignments)]
-            let mut tile_graphics_index = 0;
+            if
+                graphics_sector_memory.orientation_to_camera ==
+                crate::graphics::DirectionFromCamera::Center
+            {
+                for mut tile_properties in tile_query.iter_mut() {
 
-            for index_one in 0..crate::SECTOR_SIZE as usize {
-                for index_two in 0..crate::SECTOR_SIZE as usize {
                     match graphics_sector_memory.sector_biome {
-                        SectorBiome::Plains => {tile_graphics_index = 0;},
-                        SectorBiome::Desert => {tile_graphics_index = 12;},
-                        SectorBiome::Tundra => {tile_graphics_index = 24;},
-                        SectorBiome::Alpine => {tile_graphics_index = 36;},
+                        SectorBiome::Plains => {
+                            tile_graphics_index = 0;
+                        }
+                        SectorBiome::Desert => {
+                            tile_graphics_index = 12;
+                        }
+                        SectorBiome::Tundra => {
+                            tile_graphics_index = 24;
+                        }
+                        SectorBiome::Alpine => {
+                            tile_graphics_index = 36;
+                        }
                     }
 
-                    match graphics_sector_memory.tile_array [index_one] [index_two] {
-                        
-                        TileType::Vegetated => {tile_graphics_index += 4;},
-                        TileType::Elevated => {tile_graphics_index += 8;},
-                        _ => {},
+                    match graphics_sector_memory.tile_array[tile_properties.0.x as usize][tile_properties.0.y as usize] {
+                        TileType::Vegetated => {
+                            tile_graphics_index += 4;
+                        }
+                        TileType::Elevated => {
+                            tile_graphics_index += 8;
+                        }
+                        _ => {}
+                    }
+
+                    match graphics_sector_memory.tile_array_variety[tile_properties.0.x as usize][tile_properties.0.y as usize].0 {
+
+                        1 => {
+                            tile_graphics_index += 1;
+                        }
+                        2 => {
+                            tile_graphics_index += 2;
+                        }
+                        3 => {
+                            tile_graphics_index += 3;
+                        }
+                        _ => {}
 
                     }
 
+                    tile_properties.1.index = tile_graphics_index as usize;
                     
-                    match graphics_sector_memory.tile_array_variety [index_one] [index_two].0 {
-                        
-                        1 => {tile_graphics_index += 1;},
-                        2 => {tile_graphics_index += 2;},
-                        3 => {tile_graphics_index += 3;},
-                        _ => {},
-
-                    }
-
-        
-                    sprite_transform = Transform::from_xyz(
-                        ((((index_one as f32) - 50.0) * 96.0) as f32) + (1.5*96.0*(SECTOR_SIZE as f32) * (graphics_sector_memory.sector_coordinates.0 as f32)),
-                        ((((index_two as f32) - 50.0) * 96.0) as f32) + (1.5*96.0*(SECTOR_SIZE as f32) * (graphics_sector_memory.sector_coordinates.1 as f32)),
-                        0.0
-                    );
-
-                    sprite_transform.rotate_z(
+                    
+                    tile_properties.2.rotate_z(
                         1.5708 *
                             (
-                                graphics_sector_memory.tile_array_variety[index_one]
-                                    [index_two].1 as f32
+                                graphics_sector_memory.tile_array_variety[tile_properties.0.x as usize]
+                                    [tile_properties.0.y as usize].1 as f32
                             )
                     );
 
-                    commands.spawn(SpriteSheetBundle {
-                        sprite: TextureAtlasSprite::new(tile_graphics_index),
-                        texture_atlas: env_texture_atlas_handle.clone(),
-                        transform: sprite_transform,
-                        ..default()
-                    });
                 }
             }
-        }
 
-        tile_control.gamesector_drawn = true;
+            tile_control.gamesector_drawn = true;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -34,12 +34,12 @@ fn main() {
         .add_plugins(simulation::HiveboticaSimulationPluginGroup)
         // Everything below this in this expression is test code
         .add_systems(Startup, testing_mode_startup)
-        .add_systems(Startup, graphics::testing_mode_tile_map::tile_texture_loader)
+        .add_systems(Startup, (graphics::testing_mode_tile_map::tile_texture_loader, graphics::testing_mode_tile_map::testing_mode_spawn_tile_map).chain())
         .add_systems(
             Update,
             simulation::testing_mode_simtographics_copier::testing_mode_simtographics_processor_copier
         )
-        .add_systems(Update, graphics::testing_mode_tile_map::testing_mode_tile_map)
+        .add_systems(Update, graphics::testing_mode_tile_map::testing_mode_update_tile_map)
         .add_event::<GenerateNewSector>()
         .init_resource::<graphics::testing_mode_tile_map::EnvironmentalTextureHandle>()
         .init_resource::<graphics::testing_mode_tile_map::TileControlForSectorSwitch>()

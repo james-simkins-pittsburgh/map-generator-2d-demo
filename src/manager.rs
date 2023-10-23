49229d1035d26352e8ff13c7123d5c5896872775
jsimkins13 <jamesmsimkins@gmail.com>
2023-10-22 21:32:50 -0400

    Update manager.rs
    Working on a full check list.

--- src/manager.rs
+++ src/manager.rs
@@ -4,14 +4,33 @@ use bevy::prelude::*;
 
 #[derive(Resource, Default)]
 pub struct TaskChecklist {
-    pub simulation_ticks: [[[bool; 20]; 11]; 11],
+
+    // This is the checklist to ensure all 121 updates are applied.
+    pub simulation_updates: [[bool; 11]; 11],
+    // This indicated whether or not all updates are finished.
+    pub all_updates_finished: bool,
+    // This is the checklist to ensure al 121 worlds are ticked 20 times.
+    pub simulation_ticks: [[[[bool; 2]; 11]; 11]; 20],
+    // This indicates whether or not all ticks are finished.
     pub all_ticks_finished: bool,
+    // This indicates when player moves have been sent.
+    pub player_moves_sent: [[bool; 11]; 11],
+    // This indicates when all player moves have been sent. 
+    pub all_player_moves_sent: bool,
+    // This indicated when server updates have been sent.
+    pub sector_updates_sent: [[bool; 11]; 11],
+    // This indicated when all server updates have been sent.
+    pub all_sector_updates_sent: bool,
+
 }
 
 #[derive(Resource, Default)]
 pub struct ToDoList {
-    pub step_stimulation: [[bool; 11]; 11],
-    pub all_ticks_finished: bool,
+
+    pub update_simulation: [[bool; 11]; 11],
+    pub step_stimulation: [[[bool; 2]; 11]; 11],
+    pub send_player_moves: [[bool; 11]; 11],
+    pub send_sector_updates: [[bool; 11]; 11], 
 
 }
 

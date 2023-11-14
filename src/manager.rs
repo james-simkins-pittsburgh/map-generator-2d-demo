8674a5dee2c28deffd9b612eea07adcf3c9a05bd
jsimkins13 <jamesmsimkins@gmail.com>
2023-11-13 21:37:54 -0500

    Progress on Manager Module
    Incorporated timing into the manager module

--- src/manager.rs
+++ src/manager.rs
@@ -5,7 +5,8 @@ use crate::utility::timer;
 
 #[derive(Resource, Default)]
 pub struct TaskChecklist {
-
+    // This is the unix start time of the turn
+    pub turn_start_time: u128,
     // This is the checklist to ensure all 121 updates are applied.
     pub simulation_updates: [[bool; 11]; 11],
     // This indicated whether or not all updates are finished.
@@ -16,27 +17,71 @@ pub struct TaskChecklist {
     pub all_ticks_finished: bool,
     // This indicates when player moves have been sent.
     pub player_moves_sent: [[bool; 11]; 11],
-    // This indicates when all player moves have been sent. 
+    // This indicates when all player moves have been sent.
     pub all_player_moves_sent: bool,
-    // This indicated when server updates have been sent.
+    // This indicates when server updates have been sent.
     pub sector_updates_sent: [[bool; 11]; 11],
-    // This indicated when all server updates have been sent.
+    // This indicates when all server updates have been sent.
     pub all_sector_updates_sent: bool,
-
+    // This indicated when new sector_updated have been received.
+    pub sector_updates_received: [[bool; 11]; 11],
+    // This indicates when all server updates have been received.
+    pub all_sector_updates_received: bool,
 }
 
 #[derive(Resource, Default)]
 pub struct ToDoList {
-
     pub update_simulation: [[bool; 11]; 11],
     pub step_stimulation: [[[bool; 2]; 11]; 11],
     pub send_player_moves: [[bool; 11]; 11],
-    pub send_sector_updates: [[bool; 11]; 11], 
-
+    pub send_sector_updates: [[bool; 11]; 11],
 }
 
-fn set_tasks(mut task_checklist: ResMut<TaskChecklist>, mut to_do_list: ResMut<ToDoList>, ntp_adjustment: Res<crate::utility::timer::NTPAdjustment>) {
+fn set_tasks(
+    mut task_checklist: ResMut<TaskChecklist>,
+    mut to_do_list: ResMut<ToDoList>,
+    ntp_adjustment: Res<crate::utility::timer::NTPAdjustment>
+) {
+    // This sets the time of the turn to the unix time UTC.
 
-let unix_time = timer::universal_unix_mill_time_is(ntp_adjustment.system_clock_error);
+    let mut unix_turn_time = timer::universal_unix_mill_time_is(ntp_adjustment.system_clock_error);
 
+    // This sets the delta time of the turn based on the turn start time.
+
+    let mut unix_turn_delta_time: u128;
+
+    if unix_turn_time > task_checklist.turn_start_time {
+        unix_turn_delta_time = unix_turn_time - task_checklist.turn_start_time;
+    } else {
+        unix_turn_delta_time = 0;
+    }
+
+    // This checks to see if it is time turn start a new turn.
+    // If it is time to start a new turn it sets the turn time to 2000 more than the previous time.
+
+    if unix_turn_delta_time > 2000 && task_checklist.all_sector_updates_received {
+        // This advances the turn start time by 2 seconds (2000 milliseconds).
+
+        task_checklist.turn_start_time = task_checklist.turn_start_time + 2000;
+
+        // This resets delta time based on the new turn start time.
+
+        if unix_turn_time > task_checklist.turn_start_time {
+            unix_turn_delta_time = unix_turn_time - task_checklist.turn_start_time;
+        } else {
+            unix_turn_delta_time = 0;
+        }
+
+        // This will set all the values for the checklists to 0.
+
+        for index_1 in 0..11 {
+            for index_2 in 0..11 {
+                task_checklist.simulation_updates[index_1][index_2] = false;
+            }
+        }
+
+
+
+
+    }
 }
--- src/utility/timer.rs
+++ src/utility/timer.rs
@@ -1,43 +1,38 @@
 use bevy::prelude::*;
 use std::time::SystemTime;
 
-
 #[derive(Resource, Default)]
 pub struct NTPAdjustment {
     pub system_clock_error: i64,
 }
 
 pub fn check_ntp_time(mut ntp_adjustment: ResMut<NTPAdjustment>) {
-
     // This is just test code for before I add multiplayer. Eventually, this will need to consult a NTP server.
     ntp_adjustment.system_clock_error = 0;
 
+    if ntp_adjustment.system_clock_error > 600000 || ntp_adjustment.system_clock_error < -600000 {
+        panic!("System clock more than 10 minutes off from UTC")
+    }
 }
 
 // Unix time is the number of milliseconds since 12:00 am UTC on 1/1/70.
 
-pub fn universal_unix_mill_time_is (system_clock_error: i64) -> u128 {
-
+pub fn universal_unix_mill_time_is(system_clock_error: i64) -> u128 {
     let system_unix_time: u128;
     let universal_unix_time: u128;
-   
+
     match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(ut) => {system_unix_time = ut.as_millis();},
-        Err(_) => panic!("System time is before Unix epoch."), 
+        Ok(ut) => {
+            system_unix_time = ut.as_millis();
+        }
+        Err(_) => panic!("System time is before Unix epoch."),
     }
 
     if system_clock_error < 0 {
-
-    universal_unix_time = system_unix_time.wrapping_sub(((system_clock_error * -1) as u128));
-
-    } else { 
-
-    universal_unix_time = system_unix_time.wrapping_add((system_clock_error as u128));
-
+        universal_unix_time = system_unix_time.wrapping_sub((system_clock_error * -1) as u128);
+    } else {
+        universal_unix_time = system_unix_time.wrapping_add(system_clock_error as u128);
     }
-    
-universal_unix_time
 
+    universal_unix_time
 }
-
-
